50c3693da04e4c7da48657f0d43fd8a42b2fbd76
ferran <ferranespuna@gmail.com>
2025-12-22 10:38:27 +0100

    4 done

--- 4.py
+++ 4.py
@@ -2,11 +2,11 @@ from itertools import count, islice
 from collections import deque
 import sys
 
-def sliding_window(it, n):
-    window = deque(islice(it, n - 1), maxlen=n)
-    for x in it:
-        window.append(x)
-        yield tuple(window)
+def sliding_window(li, n):
+    if n > len(li):
+        return iter([])
+    for i in range(0, len(li) - n + 1):
+        yield tuple(li[ i: i+n])
 
 def accessible(top, x, bottom):
     n = len(x)
@@ -17,21 +17,33 @@ def accessible(top, x, bottom):
 def accessible_row(top, x, bottom):
     return ''.join(accessible(top, x, bottom))
 
-def update(it):
-    triples = sliding_window(padded, 3)
+def update(li):
+    triples = sliding_window(pad(li), 3)
     yield from map(lambda x: accessible_row(* x),  triples)
 
-def pad(it):
-    first = next(it)
-    yield '.' * (len(first) + 2)
-    yield '.' + first + '.'
-    for x in it:
-        yield '.' + x + '.'
-    yield '.' * (len(first) + 2)
+def reduce(li):
+
+    current_xs = sum(row.count('x') for row in li)
+    li_next = list(update(li))
+    next_xs = sum(row.count('x') for row in li_next)
+    if next_xs != current_xs:
+        return reduce(li_next)
+    else:
+        return li_next
+
+def pad(li):
+    l = len(li[0])
+    return ['.' * (l + 2)] + ['.' + x + '.' for x in li] + ['.' * (l + 2)]
 
 if __name__ == "__main__":
     inputs = map(str.rstrip, sys.stdin)
-    padded = pad(inputs)
+    grid = list(inputs)
+
     print(sum(
-        u.count('x') for u in update(padded)
+        u.count('x') for u in update(grid)
     ))
+
+    print(sum(
+        u.count('x') for u in reduce(grid)
+    ))
+
